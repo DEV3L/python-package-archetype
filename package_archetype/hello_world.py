3f0f7426a361f498cb4eaee075568a82cbf7b0e4
Justin Beall <jus.beall@gmail.com>
2016-08-02 09:07:22 -0400

    Update version for example

--- package_archetype/hello_world.py
+++ package_archetype/hello_world.py
@@ -1,4 +1,5 @@
-HELLO_WORLD_MESSAGE = 'Hello world! PyOhio Demo!'
+HELLO_WORLD_MESSAGE = 'Hello world! PyOhio Demo - 2!'
+
 
 def get_message():
     return HELLO_WORLD_MESSAGE
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='package_archetype',
     packages=find_packages(),
-    version='0.3',
+    version='0.4',
     description='A simple lib that prints out hello world',
     author='Justin Beall',
     author_email='jus.beall@gmail.com',
     url='https://github.com/DEV3L/python-package-archetype',
-    download_url='https://github.com/DEV3L/python-package-archetype/tarball/0.3',
+    download_url='https://github.com/DEV3L/python-package-archetype/tarball/0.4',
     keywords=['dev3l', 'archetype', 'pypi', 'package'],  # arbitrary keywords
     install_requires=[
         'pytest==2.9.2'
